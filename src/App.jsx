f65acaeafed6787601fa3eea9977b164b1e39a65
roonyi@gmail.com <WRYI3sfhk>
2023-10-20 15:59:20 -0500

    intro v8

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchServerData } from '/src/helpers/mondayData';
 import { buildWebDetail } from '/src/helpers/viewHelper';
 import { Home } from '/src/components/home';
-//import { Product } from '/src/components/product';
-//import { Solutions } from '/src/components/solutions';
+import { Aboutus } from '/src/components/aboutus';
+import { Faq } from '/src/components/faq';
 //import { Pricing } from '/src/components/pricing';
 //import { Faq } from '/src/components/faq';
 //import { Messenger } from '/src/components/messenger';
@@ -43,9 +43,9 @@ function App() {
     <BrowserRouter>
        <Routes>
         <Route path="/" element={<Home header={detail?.header} footer={detail?.footer} home={detail?.home} />} />
-{/*        <Route path="/product" element={<Product header={detail?.header} footer={detail?.footer} product={detail?.product} />} />
-        <Route path="/solutions" element={<Solutions header={detail?.header} footer={detail?.footer} solutions={detail?.solutions} />} />
-        <Route path="/pricing" element={<Pricing header={detail?.header} footer={detail?.footer} pricing={detail?.pricing} />}/>
+        <Route path="/aboutus" element={<Aboutus header={detail?.header} footer={detail?.footer} aboutus={detail?.aboutus} />} />
+        <Route path="/faq" element={<Faq header={detail?.header} footer={detail?.footer} faq={detail?.faq} />} />
+{/*        <Route path="/pricing" element={<Pricing header={detail?.header} footer={detail?.footer} pricing={detail?.pricing} />}/>
         <Route path="/faq" element={<Faq header={detail?.header} footer={detail?.footer} faq={detail?.faq} />}   />
         <Route path="/privacy" element={<Workdoc header={detail?.header} footer={detail?.footer} workdoc={'privacy'} />}/>
         <Route path="/contact" element={<Workdoc header={detail?.header} footer={detail?.footer} workdoc={'contact'} />} />
--- src/components/aboutus.jsx
+++ src/components/aboutus.jsx
@@ -5,9 +5,9 @@ import { FooterMenu } from "./footer";
 //import Messenger from './messenger'; // for loading porpuses
 import WebContent from './content';
 
-export const Home = (props) => {
+export const Aboutus = (props) => {
     console.log("props.header", props.header)
-    const HomeBlock = (props) => {
+    const AboutusBlock = (props) => {
         return (
             <Box className={'ni-layout-home-block'}  >
                 <WebContent info={props.detail}  contentClassName={props.contentClassName}/>
@@ -15,32 +15,32 @@ export const Home = (props) => {
         );
     }
 
-    const HomeIntro = (props) => {
-/*        let intros = props.content?.intro.component?.elements
-        console.log("intros: ", intros)
-        console.log ("intros: ", Object.keys(intros))
-        Object.keys(intros).map((key) => {
-            if (intros[key].render !== 'Asset'){
-                console.log("intros_right: ", intros[key].render)
-            }else {
-                console.log("intros_left: ", intros[key].render)
-            }
-        })  */
-        console.log("props.content?.intro_left.component?.elements: ", props.content?.intro_left.component?.elements)
+    const AboutusIntro = (props) => {
+        console.log("props.content?.who_we_are_left.component?.elements: ", props.content?.who_we_are_left.component?.elements)
         return (
             <Flex
                 justify={Flex.justify.CENTER} 
                 align={Flex.justify.CENTER} 
                 className={'ni-layout-home-intro'} 
                 direction={Flex.directions.ROW}
-                gap={Flex.gaps.SMALL} >
-                <HomeBlock detail={props.content?.intro_left.component?.elements} contentClassName={'ni-layout-home-intro-title-text'}/>
-                <HomeBlock detail={props.content?.intro_right.component?.elements} />
+                gap={Flex.gaps.SMALL} 
+                >
+                <AboutusBlock detail={props.content?.who_we_are_left.component?.elements} contentClassName={'ni-layout-home-intro-title-text'}/>                
+                <Flex
+                justify={Flex.justify.CENTER} 
+                align={Flex.justify.START} 
+                className={'ni-layout-who-we-are-intro'} 
+                direction={Flex.directions.COLUMN}
+                gap={Flex.gaps.SMALL} 
+                >
+                <AboutusBlock detail={props.content?.who_we_are_right.component?.elements} contentClassName={'ni-layout-home-intro-text'} />
+                </Flex>
             </Flex>
+            
         );
     };
 
-    const HomeApproach_title = (props) => {
+    const AboutusApproach_title = (props) => {
         console.log("props.content?.approach_title.component?.elements: ", props.content?.approach_title.component?.elements)
         return (
             <Flex
@@ -49,13 +49,13 @@ export const Home = (props) => {
                 className={'ni-layout-home-approach'} 
                 direction={Flex.directions.ROW}
                 gap={Flex.gaps.SMALL} >
-                <HomeBlock detail={props.content?.approach_title.component?.elements} contentClassName={'ni-layout-home-approach-title-text'}/>
+                <AboutusBlock detail={props.content?.approach_title.component?.elements} contentClassName={'ni-layout-home-approach-title-text'}/>
             </Flex>
         );
     };
 
-    const HomeApproach_cards = (props) => {
-        console.log("props.content?.approach_cards.component?.elements: ", props.content?.approach_cards.component?.elements)
+    const AboutusApproach_cards = (props) => {
+        //console.log("props.content?.approach_cards.component?.elements: ", props.content?.approach_cards.component?.elements)
         return (
             <Flex
                 justify={Flex.justify.CENTER} 
@@ -63,27 +63,13 @@ export const Home = (props) => {
                 className={'ni-layout-home-approach'} 
                 direction={Flex.directions.ROW}
                 gap={Flex.gaps.SMALL} >
-                <HomeBlock detail={props.content?.approach_cards.component?.elements} />
+                <AboutusBlock detail={props.content?.approach_cards.component?.elements} />
             </Flex>
         );
     };
 
-    const HomeProducts_title = (props) => {
-        console.log("props.content?.products_title.component?.elements: ", props.content?.products_title.component?.elements)
-        return (
-            <Flex
-                justify={Flex.justify.CENTER} 
-                align={Flex.justify.CENTER} 
-                className={'ni-layout-home-products'} 
-                direction={Flex.directions.COLUMN}
-                gap={Flex.gaps.SMALL} >
-                <HomeBlock detail={props.content?.products_title.component?.elements} contentClassName={'ni-layout-home-products-title'}/>
-            </Flex>
-        );
-    };
-
-    const HomeProducts = (props) => {
-        console.log("props.content?.products.component?.elements: ", props.content?.products.component?.elements)
+    const AboutusTeam_title = (props) => {
+        console.log("props.content?.team_title.component?.elements: ", props.content?.team_title.component?.elements)
         return (
             <Flex
                 justify={Flex.justify.CENTER} 
@@ -91,7 +77,21 @@ export const Home = (props) => {
                 className={'ni-layout-home-products'} 
                 direction={Flex.directions.ROW}
                 gap={Flex.gaps.SMALL} >
-                <HomeBlock detail={props.content?.products.component?.elements} />
+                <AboutusBlock detail={props.content?.team_title.component?.elements} contentClassName={'ni-layout-home-products-title'}/>
+            </Flex>
+        );
+    };
+
+    const AboutusTeam = (props) => {
+        console.log("props.content?.team.component?.elements: ", props.content?.team.component?.elements)
+        return (
+            <Flex
+                justify={Flex.justify.CENTER} 
+                align={Flex.justify.CENTER} 
+                className={'ni-layout-home-products'} 
+                direction={Flex.directions.COLUMN}
+                gap={Flex.gaps.SMALL} >
+                <AboutusBlock detail={props.content?.team.component?.elements} />
             </Flex>
         );
     };
@@ -107,15 +107,15 @@ export const Home = (props) => {
                 direction={Flex.directions.COLUMN} 
                 gap={Flex.gaps.NONE} >
                 <HeaderMenu content={props.header} />
-                <HomeIntro content={props.home} />
-                <HomeApproach_title content={props.home} />
-                <HomeApproach_cards content={props.home} />
-                <HomeProducts_title content={props.home} />
-                <HomeProducts content={props.home} />
+                <AboutusIntro content={props.aboutus} />
+                <AboutusApproach_title content={props.aboutus} />
+                <AboutusApproach_cards content={props.aboutus} />
+                <AboutusTeam_title content={props.aboutus} />
+                <AboutusTeam content={props.aboutus} />
                 <FooterMenu content={props.footer} />
 
             </Flex>
     );
 }
 
-export default Home;
+export default Aboutus;
