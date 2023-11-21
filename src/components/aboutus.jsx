0738840109307666b43ef443ddd1fa2d334367fd
roonyi@gmail.com <WRYI3sfhk>
2023-11-21 14:16:49 -0500

    carousel v4

--- src/components/aboutus.jsx
+++ src/components/aboutus.jsx
@@ -5,10 +5,31 @@ import { FooterMenu } from "./footer";
 //import Messenger from './messenger'; // for loading porpuses
 import WebContent from './content';
 import { Container, Row, Col } from 'react-bootstrap';
+import Carousel from 'react-multi-carousel';
+import "react-multi-carousel/lib/styles.css";
 
 export const Aboutus = (props) => {
     console.log("props.header", props.header)
-    
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+          },
+          desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+          },
+          tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+          },
+          mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+          }
+      };
+
     const AboutusIntro = (props) => {
         console.log("props.content?.who_we_are_left.component?.elements: ", props.content?.who_we_are_left.component?.elements)
         return (
@@ -75,16 +96,35 @@ export const Aboutus = (props) => {
                 className={'ni-layout-card-approach'} 
                 direction={Flex.directions.ROW}
                 gap={Flex.gaps.SMALL} >
-                <Container fluid>
-                    <Row
-                    align={Flex.justify.CENTER}
-                    >
-                        {/* <Flex
-                            className={'ni-layout-card-approach'} 
-                            direction={Flex.directions.ROW}
-                            gap={Flex.gaps.SMALL} > */}
-                            <WebContent info={props.content?.approach_cards.component?.elements} alterClassHint={'card'}/>
-                        {/* </Flex> */}
+                <Container fluid
+                align={Flex.justify.CENTER}
+                >
+                    <Row>
+                        {/* <WebContent info={props.content?.approach_cards.component?.elements} alterClassHint={'card'}/> */}
+                        <Col>
+                            {props.content?.approach_cards.component?.elements && 
+                            <Carousel 
+                            responsive={responsive}
+                            infinite={true}
+                            // autoPlay={true}
+                            // autoPlaySpeed={3000}
+                            arrows={true}
+                            showDots={true}
+                            // sliderClass={'carousel'}
+                            containerClass={'carousel-container'}
+                            dotListClass={'carousel-dotlist'}
+                            // renderDotsOutside={true}
+                            // removeArrowOnDeviceType={["desktop", "mobile", "tablet" ]}
+                            removeArrowOnDeviceType={["superLargeDesktop","desktop"]}
+                            >
+                                {
+                                props.content?.approach_cards.component?.elements?.map((element) => {
+                                let elementArray = [element];
+                                return (<WebContent info={elementArray} alterClassHint={'card'}/>)
+                                    })
+                                }
+                            </Carousel>}
+                        </Col>
                     </Row>
                 </Container>
             </Flex>
@@ -100,25 +140,42 @@ export const Aboutus = (props) => {
                 className={'ni-layout-home-products'} 
                 direction={Flex.directions.ROW}
                 gap={Flex.gaps.SMALL} >
-                <Container fluid>
-                    <Row
-                    align={Flex.justify.CENTER}>
-                    <Col sm="3" lg="3">
-                        <Flex
-                        className={'ni-layout-home-products'} 
-                        direction={Flex.directions.COLUMN}
-                        gap={Flex.gaps.SMALL} >
-                            <WebContent info={props.content?.team_title.component?.elements} contentClassName={'ni-layout-home-products-title'}/>
-                        </Flex>
-                    </Col>
-                    {/* <Col lg="2">
-                        <Flex
-                        className={'ni-layout-home-products'} 
-                        direction={Flex.directions.ROW}
-                        gap={Flex.gaps.SMALL} > */}
-                            <WebContent info={props.content?.team.component?.elements} alterClassHint={'card'}/>
-                        {/* </Flex>
-                    </Col> */}
+                <Container fluid
+                align={Flex.justify.CENTER}>
+                    <Row>
+                        <Col sm="3" lg="3" md="12">
+                            <Flex
+                            className={'ni-layout-home-products'} 
+                            direction={Flex.directions.COLUMN}
+                            gap={Flex.gaps.SMALL} >
+                                <WebContent info={props.content?.team_title.component?.elements} contentClassName={'ni-layout-home-products-title'}/>
+                            </Flex>
+                        </Col>
+                        <Col lg="9">
+                            {/* <WebContent info={props.content?.team.component?.elements} alterClassHint={'card'}/> */}
+                            {props.content?.team.component?.elements && 
+                            <Carousel 
+                            responsive={responsive}
+                            infinite={true}
+                            // autoPlay={true}
+                            // autoPlaySpeed={3000}
+                            arrows={true}
+                            showDots={true}
+                            // sliderClass={'carousel'}
+                            containerClass={'carousel-container'}
+                            dotListClass={'carousel-dotlist'}
+                            // renderDotsOutside={true}
+                            // removeArrowOnDeviceType={["desktop", "mobile", "tablet" ]}
+                            removeArrowOnDeviceType={["superLargeDesktop","desktop"]}
+                            >
+                                {
+                                props.content?.approach_cards.component?.elements?.map((element) => {
+                                let elementArray = [element];
+                                return (<WebContent info={elementArray} alterClassHint={'card'}/>)
+                                    })
+                                }
+                            </Carousel>}
+                        </Col>
                     </Row>
                 </Container>    
             </Flex>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -120,45 +120,32 @@ export const Home = (props) => {
                 <Container fluid
                     align={Flex.justify.CENTER}
                     >
-                    {/* <Carousel>                        
-                       {props.content?.approach_cards.component?.elements?.map((element) => {
-                        let card_content0 = element.content.split("##");
-                        return(
-                        <Carousel.Item>
-                            <div className='card' style={{height: '300px', width:'200px'}}>
-                                            <img src= {element.files[0]} className={`ni-layout-card-image`}/>
-                                            <div className='card-body'>
-                                                <h5 className='card-title'>{card_content0[0]}</h5>
-                                                <h6 className='card-text'>{card_content0[1]}</h6>
-                                            </div>
-                            </div>
-                        </Carousel.Item> 
-                        )
-                            })}
-                    </Carousel> */}
-                    {console.log('element_cards1',props.content?.approach_cards.component?.elements, props.content?.approach_cards.component?.elements.length)}
-                    {props.content?.approach_cards.component?.elements && 
-                    <Carousel 
-                    responsive={responsive}
-                    infinite={true}
-                    // autoPlay={true}
-                    // autoPlaySpeed={3000}
-                    arrows={true}
-                    showDots={true}
-                    // sliderClass={'carousel'}
-                    containerClass={'carousel-container'}
-                    dotListClass={'carousel-dotlist'}
-                    // renderDotsOutside={true}
-                    // removeArrowOnDeviceType={["desktop", "mobile", "tablet" ]}
-                    // customLeftArrow={'<'}
-                    >
-                        {
-                        props.content?.approach_cards.component?.elements?.map((element) => {
-                        let elementArray = [element];
-                        return (<WebContent info={elementArray} alterClassHint={'card'}/>)
-                            })
-                        }
-                    </Carousel>}
+                    <Row >
+                        <Col>
+                            {props.content?.approach_cards.component?.elements && 
+                            <Carousel 
+                            responsive={responsive}
+                            infinite={true}
+                            // autoPlay={true}
+                            // autoPlaySpeed={3000}
+                            arrows={true}
+                            showDots={true}
+                            // sliderClass={'carousel'}
+                            containerClass={'carousel-container'}
+                            dotListClass={'carousel-dotlist'}
+                            // renderDotsOutside={true}
+                            // removeArrowOnDeviceType={["superLargeDesktop","desktop", "mobile", "tablet" ]}
+                            removeArrowOnDeviceType={["superLargeDesktop","desktop"]}
+                            >
+                                {
+                                props.content?.approach_cards.component?.elements?.map((element) => {
+                                let elementArray = [element];
+                                return (<WebContent info={elementArray} alterClassHint={'card'}/>)
+                                    })
+                                }
+                            </Carousel>}
+                            </Col>
+                        </Row>
                 </Container>
             </Flex>
         );
@@ -178,10 +165,34 @@ export const Home = (props) => {
                 align={Flex.justify.CENTER}
                 >
                         <Row >   
-                            <Col>
+                            <Col lg="3">
                                 <WebContent info={props.content?.products_title.component?.elements} contentClassName={'ni-layout-home-products-title'}/>
                             </Col>
-                            <WebContent info={props.content?.products.component?.elements} alterClassHint={'card'}/>
+                            {/* <WebContent info={props.content?.products.component?.elements} alterClassHint={'card'}/> */}
+                            <Col lg="9">
+                            {props.content?.approach_cards.component?.elements && 
+                            <Carousel 
+                            responsive={responsive}
+                            infinite={true}
+                            // autoPlay={true}
+                            // autoPlaySpeed={3000}
+                            arrows={true}
+                            showDots={true}
+                            // sliderClass={'carousel'}
+                            containerClass={'carousel-container'}
+                            dotListClass={'carousel-dotlist'}
+                            // renderDotsOutside={true}
+                            // removeArrowOnDeviceType={["desktop", "mobile", "tablet" ]}
+                            removeArrowOnDeviceType={["superLargeDesktop","desktop"]}
+                            >
+                                {
+                                props.content?.products.component?.elements?.map((element) => {
+                                let elementArray = [element];
+                                return (<WebContent info={elementArray} alterClassHint={'card'}/>)
+                                    })
+                                }
+                            </Carousel>}
+                            </Col>
                         </Row>
                 </Container>
             </Flex>
